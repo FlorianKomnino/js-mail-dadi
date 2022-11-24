bef06f8d05962ea8b22f349bdc2ea1b1dda731ec
FlorianKomnino <florian.komnino@outlook.it>
2022-11-24 15:21:33 +0100

    completed dice game in console

--- js/script.js
+++ js/script.js
@@ -3,14 +3,23 @@ console.log('Hello World!');
 // $ Gioco dei dadi
 
 // $ Genero un numero random da 1 a 6 per il giocatore
-const userRandomSixDiceResult(Math.floor((Math.random() * 6) + 1));
+const userRandomSixDiceResult = (Math.floor((Math.random() * 6) + 1));
+console.log('Il risultato del tuo tiro è:' + userRandomSixDiceResult)
 
 // $ Genero un numero random da 1 a 6 per il computer
-const computerRandomSixDiceResult(Math.floor((Math.random() * 6) + 1));
+const computerRandomSixDiceResult = (Math.floor((Math.random() * 6) + 1));
+console.log('Il risultato del tiro del computer è:' + computerRandomSixDiceResult)
 
 // $ Stabilisco il vincitore in base al relativo numero maggiore
-
-
+if (userRandomSixDiceResult > computerRandomSixDiceResult) {
+    console.log('Complimenti!!! Hai Vinto!!!')
+}
+else if (userRandomSixDiceResult < computerRandomSixDiceResult) {
+    console.log('Ritenta, sarai più fortunato.')
+}
+else if (userRandomSixDiceResult === computerRandomSixDiceResult) {
+    console.log('Giochiamo ancora?')
+}
 
 // % Mail
 // % Chiedo all'utente di inserire la sua e-mail in un input
