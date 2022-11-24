05f358fc6d56d3ca66de9b10793e285b2a3e3ed4
FlorianKomnino <florian.komnino@outlook.it>
2022-11-24 15:32:00 +0100

    added html elements for login chec and email input

--- js/script.js
+++ js/script.js
@@ -22,6 +22,7 @@ else if (userRandomSixDiceResult === computerRandomSixDiceResult) {
 }
 
 // % Mail
+    // % creo un array di email
 // % Chiedo all'utente di inserire la sua e-mail in un input
 // % Verifico che l'e-mail inserita faccia parte della lista
 // % Stampo messaggio di accesso, consentito o negato in base ai casi
