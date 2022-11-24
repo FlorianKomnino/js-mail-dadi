06a919c60b7ae040dab9fea63c0d8a5ef4fdfcb1
FlorianKomnino <florian.komnino@outlook.it>
2022-11-24 14:42:43 +0100

    added comments of to-do list in js, started adding elements and style where to make it happen

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 console.log('Hello World!');
 
+// $ Gioco dei dadi
+// $ Genero un numero random da 1 a 6 per il giocatore
+// $ Genero un numero random da 1 a 6 per il computer
+// $ Stabilisco il vincitore in base al relativo numero maggiore
+
+
+
+// % Mail
+// % Chiedo all'utente di inserire la sua e-mail in un input
+// % Verifico che l'e-mail inserita faccia parte della lista
+// % Stampo messaggio di accesso, consentito o negato in base ai casi
+
